b141ad3040ddc0d1ba8dbc7d91d91ba0a5cc7042
Salvador-dp <dp240093@alumno.udb.edu.sv>
2025-10-23 16:44:54 -0600

    Nuevas dependencias
    Con estas dependencias ya funciona con los cambios en MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     //GSON converter
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.airbnb.android:lottie:6.1.0") //Para las dependencias
+
+    // OkHttp (cliente HTTP + interceptor para logs)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
